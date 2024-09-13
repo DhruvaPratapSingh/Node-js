faae0fa77e36ecc9abe7514db3074a4801e30e35
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-09-13 12:12:03 +0530

    get http methode

--- Http.js
+++ Http.js
@@ -9,12 +9,54 @@
 // })
 
 // assignment 1;
+// const http=require("http");
+// const fs=require("fs");
+// const server=http.createServer((req,res)=>{
+//     if(req.url==="/favicon.ico") return res.end();
+//     const log=`no one`;
+//     fs.appendFile("./test.txt",log,(err,data)=>{
+//      switch(req.url){
+//         case "/":
+//             res.end("Home page");
+//             break;
+//         case "/about":
+//             res.end("about page");
+//             break; 
+//         default :
+//             res.end("page not found !")  
+//             break;     
+//     }
+// });
+// })
+// server.listen(4000,()=>{
+//     console.log(`server is ruuning at port ${4000}`);
+// })
 const http=require("http");
 const fs=require("fs");
-const server=http.createServer((req,res)=>{
-const log=`hello ${req.url}\n`;
-fs.appendFile("./test.txt",log,(err,data)=>{
-    res.end("good luck!");
+const url =require("url");
+// http://localhost:4000/about?myname=dhes&userid=1
+const server=http.createServer((req,res)=>{ 
+    if(req.url==="/favicon.ico") return res.end();
+    const myurl=url.parse(req.url,true);
+    const log=`hello ${req.url} new req recieved\n`
+    console.log(myurl);
+    fs.appendFile("./test.txt",log,(err,data)=>{
+     switch(myurl.pathname){
+        case "/":
+            res.end("Home page");
+            break;
+        case "/about":
+            const user=myurl.query.myname;
+            res.end(`Hi, myself ${user}`);
+            break; 
+        case "/search":
+            // http://localhost:4000/search?search_query=tic+tac+toe
+            const search=myurl.query.search_query;
+            res.end("here your serach for " + search);    
+        default :
+            res.end("page not found !")  
+            break;     
+    }
 });
 })
 server.listen(4000,()=>{
