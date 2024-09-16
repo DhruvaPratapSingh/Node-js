1a5f975ffc528c38e098e78d84c805586350cb85
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-09-16 23:49:09 +0530

    all http method with rw json data

--- project-01/index.js
+++ project-01/index.js
@@ -1,22 +1,47 @@
 const express=require("express");
-const data=require("./MOCK_DATA.json")
+const users=require("./MOCK_DATA.json")
+const fs=require("fs");
 const app=express();
 
-app.get("/api/users",(req,res)=>{
-    return res.json(data);
-});
-app.get("/api/users/:id",(req,res)=>{
-    const id=Number(req.params.id);
-    const user=data.find((user)=>user.id===id);
-    return res.json(user);
-});
+app.use(express.urlencoded({extended:false}))// middleware-plugin
+
+// app.get("/api/users",(req,res)=>{
+//     return res.json(users);
+// });
+// app.get("/api/users/:id",(req,res)=>{
+//     const id=Number(req.params.id);
+//     const user=data.find((user)=>user.id===id);
+//     return res.json(user);
+// });
 app.get("/users",(req,res)=>{
     const html=`
     <ul>
-    ${data.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
     </ul>
     `;
     res.send(html);
+})
+app
+.route("/api/users/:id")
+.get((req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id===id);
+    return res.json(user);
+})
+.patch((req,res)=>{
+    return res.json({status:"Pending"});
+})
+.delete((req,res)=>{
+    return res.json({status:"Pending"});
+})
+app.post("/api/users",(req,res)=>{
+    const body=req.body;
+    // console.log(req.body);
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        console.log("err occured to adding data in json file")
+        return res.json({status:"Success",id:users.length+1});
+    });
 })
 const PORT=8000;
 app.listen((PORT),()=>{
